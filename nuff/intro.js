488057a5477cbfcbb8ee4c42ec07cde6ec5a3dbe
relik <jeremie.dubuis@gmail.com>
2015-11-22 23:27:07 +0100

    master - added a banner and clean package.json

--- nuff/intro.js
+++ nuff/intro.js
@@ -1,3 +1,9 @@
+/**
+  * @desc Nuff.js is a simple Model Presenter framework to structure your JS code
+  * @version 0.1.0
+  * @author Jérémie Dubuis jeremie.dubuis@gmail.com
+  * @license The MIT License (MIT)
+*/
 var Nuff = function() {
 
     var Nuff = {
