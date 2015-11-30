629defae11494be1fd9163e1a1f206b82dc65784
Jérémie Dubuis <jeremie.dubuis@happyend.com>
2015-11-30 07:50:53 +0100

    master - commenting presenter

--- nuff/Presenter.js
+++ nuff/Presenter.js
@@ -1,9 +1,20 @@
+/**
+  * @desc  Nuff.Presenter is a communication hub between views and models/collections
+           The main idea is that view instanciate presenters with themselves as a parameter
+           The view implements a presenterMethods array to bind presenter methods to itself and
+           thus implicitly calls presenter actions on user input.
+           Presenters are singletons by default but can be instanciables by setting the singleton
+           attribute to false
+  * @param name (string): holds the presenter's name for further reference
+  * @param extended (object): holds data that will extend Presenter functionnality, all functions
+           will be added to object prototype whereas values will be added to object itself
+*/
+
 var Presenter = function(name, extended) {
 
 
 
     var Presenter = function(options) {
-
 
         for (var key in extended) {
             if (typeof extended[key] === 'function') Presenter.prototype[key] = extended[key];
@@ -20,6 +31,10 @@ var Presenter = function(name, extended) {
 
     Presenter.prototype= {
 
+        /**
+          * @desc _init is a local initialization function called on instanciation or reset,
+                  it allows to rebind presenter methods to a new view
+        */
         _init: function(options) {
             this.destroy();
             if (typeof options === 'object' && options.presenterMethods) this.bindMethodsToView(options, options.presenterMethods);
@@ -31,6 +46,11 @@ var Presenter = function(name, extended) {
 
         },
 
+        /**
+          * @desc  bindMethodsToView attaches presentermethods with presenter scope or (view.presenterMethodsScope)
+          * @param view (object): a view object to which methods will be bound
+          * @param presenterMethods (array) : a list of strings that will be evaluated against presenter methods
+        */
         bindMethodsToView: function(view, presenterMethods) {
             if (view)
                 this.mapViewFunctions(view, presenterMethods, view.presenterMethodsScope || this);
@@ -62,7 +82,7 @@ var Presenter = function(name, extended) {
 
     };
 
-
+    // if exented.singleton is defined check wether presenter should be singleton or instanciable
     if (extended && typeof extended.singleton !== "undefined" && !extended.singleton)
         return Presenter;
     else return function(options) {
